2b28b0f0c55cad95a4b492e770d4338f323ffe28
LeoAyala <ljvryl@gmail.com>
2026-02-15 21:16:05 -0300

    Update layout_top.php

--- str/inc/layout_top.php
+++ str/inc/layout_top.php
@@ -27,8 +27,8 @@ $bodyClass = $isClient ? ($bodyClass . ' no-nav') : $bodyClass;
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/str.css">
-    <link rel="stylesheet" href="assets/str-theme.css">
+    <link rel="stylesheet" href="assets/str.css?v=20260215">
+    <link rel="stylesheet" href="assets/str-theme.css?v=20260215">
 </head>
 <body class="<?php echo htmlspecialchars($bodyClass, ENT_QUOTES, 'UTF-8'); ?>">
 <div class="topbar">
